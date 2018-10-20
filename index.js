5f5488768f7ee700c09f791b8d1eda629f13324a
Alberico.Lima <alberico.lima@gmail.com>
2018-10-20 06:59:33 -0300

    Lecture 129 Authenticating

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
+
 const app = express();
 
 mongoose.connect('mongodb://fid:fid0201@ds119343.mlab.com:19343/itst')
@@ -19,6 +21,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users',users);
+app.use('/api/auth',auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
